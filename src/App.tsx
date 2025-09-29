b50f7fe8453584404959ab8d67bd1aee26c3e419
LeonardoFMiranda <leomiranda1997@gmail.com>
2025-09-29 15:09:55 -0300

    remocao do LINK no app inutilizavel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './pages/HomeScreen/HomeScreen';
 import DetailsScreen from './pages/DetailsScreen/DetailsScreen';
 import BuscaScreen from './pages/BuscaScreen/BuscaScreen';
